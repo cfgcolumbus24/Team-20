b655ca03371715dc0709cd2469e6348b1c7564f7
andrewtlu <andrew.lu@emory.edu>
2024-11-02 01:12:04 -0400

    Forgot to add firebase config

--- backend/src/config/firebase.config.js
+++ backend/src/config/firebase.config.js
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 import adminAccount from "../../firebaseAdminKey.json" assert { type: "json" };
 import firebaseConfig from "../../firebaseConfig.json" assert { type: "json" };
 
-export const firebaseAPIKey = firebaseConfig.apiKey;;
+export const firebaseAPIKey = firebaseConfig.apiKey;
 
 admin.initializeApp({
   credential: admin.credential.cert(adminAccount),
