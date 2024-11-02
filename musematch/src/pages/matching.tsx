bf052ad08d0734e8a478fef721c6894c001bc575
PrateekRaju <princepratu@gmail.com>
2024-11-02 06:10:46 -0400

    Revert "Format frontend and backend"
    This reverts commit 47b00cbe0f66f1097f97838917371e118d23a1cc.

--- musematch/src/pages/matching.tsx
+++ musematch/src/pages/matching.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 // import { FaHeart } from "react-icons/fa";
 import { IoMdCheckboxOutline } from "react-icons/io";
 import { RxCross2 } from "react-icons/rx";
@@ -23,43 +23,27 @@ export const Matching = () => {
   const [showModal, setShowModal] = useState(false);
   const [modalText, setModalText] = useState("");
 
-  //Load in artwork Data
-  const artworkData: Artwork[] = [
-    {
-      title: "Artwork 1",
-      description: "Description for artwork 1",
-      imgUrl: logo,
-    },
-    {
-      title: "Artwork 2",
-      description: "Description for artwork 2",
-      imgUrl: logo,
-    },
-    {
-      title: "Artwork 3",
-      description: "Description for artwork 3",
-      imgUrl: logo,
-    },
-    {
-      title: "Artwork 4",
-      description: "Description for artwork 4",
-      imgUrl: logo,
-    },
-    // { title: "Artwork 5", description: "Description for artwork 5", imgUrl: logo },
-    // { title: "Artwork 6", description: "Description for artwork 6", imgUrl: logo }
-  ];
-
-  //makes cards automatically swipe right
-  const handleHeartClick = () => {
-    setCurrentX(1500); // Move off-screen to the right
-    setRotation(40);
-    console.log("User Swiped Right");
-
-    setTimeout(() => {
-      setCurrentX(0);
-      setRotation(0);
-    }, 500);
-  };
+    //Load in artwork Data
+    const artworkData: Artwork[] = [
+        { title: "Artwork 1", description: "Description for artwork 1", imgUrl: logo },
+        { title: "Artwork 2", description: "Description for artwork 2", imgUrl: logo },
+        { title: "Artwork 3", description: "Description for artwork 3", imgUrl: logo },
+        { title: "Artwork 4", description: "Description for artwork 4", imgUrl: logo }
+        // { title: "Artwork 5", description: "Description for artwork 5", imgUrl: logo },
+        // { title: "Artwork 6", description: "Description for artwork 6", imgUrl: logo }
+      ];
+    
+    //makes cards automatically swipe right 
+    const handleHeartClick = () => {
+        setCurrentX(1500); // Move off-screen to the right
+        setRotation(40);
+        console.log("User Swiped Right");
+        
+        setTimeout(() => {
+            setCurrentX(0);
+            setRotation(0);
+          }, 500);
+    };
 
   // Automatically swipe left
   const handleXClick = () => {
@@ -85,37 +69,28 @@ export const Matching = () => {
     setShowModal(true);
   };
 
-  return (
-    <div className="App">
-      <div
-        className="card"
-        style={{
-          transform: `translateX(${currentX}px) rotate(${rotation}deg)`,
-          transition: "transform 0.3s ease-out",
-        }}
-      >
-        <img src={logo} alt="person" className="pfp-image" />
-        <h1 className="person-name">Name</h1>
-        <h2 className="about-me">About me</h2>
-        <div className="tag-container">
-          <span className="tag">Mentor</span>
-          <span className="tag">YOE</span>
-          <span className="tag">Art Type</span>
+    return (
+        <div className="App">
+            <div className="card"
+            style={{
+                transform: `translateX(${currentX}px) rotate(${rotation}deg)`,
+                transition: "transform 0.3s ease-out",
+              }}
+            >
+            <img src={logo} alt="person" className="pfp-image"/>
+            <h1 className="person-name">Name</h1>
+            <h2 className="about-me">About me</h2>
+            <div className="tag-container">
+                <span className="tag">Mentor</span>
+                <span className="tag">YOE</span>
+                <span className="tag">Art Type</span>
+            </div>
+            <div className="like-dislike-container">
+                <RxCross2 className="like-dislike" onClick={handleXClick} aria-label="Dislike"/>
+                <IoMdCheckboxOutline className="like-dislike" onClick={handleHeartClick} aria-label="Love" />
+            </div>
         </div>
-        <div className="like-dislike-container">
-          <RxCross2
-            className="like-dislike"
-            onClick={handleXClick}
-            aria-label="Dislike"
-          />
-          <IoMdCheckboxOutline
-            className="like-dislike"
-            onClick={handleHeartClick}
-            aria-label="Love"
-          />
-        </div>
-      </div>
-      {/* Artwork Section */}
+        {/* Artwork Section */}
       <div className="artwork-section">
         <h2 className="artist-title">Artist's Artwork</h2>
         <div className="artwork-gallery">
