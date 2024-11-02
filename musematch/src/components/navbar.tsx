e513bba7a30a17a3862013656b65601b91570d13
shreyamori <shreya29.m@gmail.com>
2024-11-01 20:42:38 -0400

    fix browser router

--- musematch/src/components/navbar.tsx
+++ musematch/src/components/navbar.tsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <div>
-      <Link to="/"> Home </Link>
-      <Link to="/login"> Login </Link>
+      <BrowserRouter>
+        <Link to="/"> Home </Link>
+        <Link to="/login"> Login </Link>
+        <Link to="/connections"> Connections </Link>
+      </BrowserRouter>
     </div>
   );
 };
