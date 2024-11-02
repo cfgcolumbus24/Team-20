344f246d3210e2d030425edce4049c2e9fa6167d
Tunmise Kehinde <tunmisekehinde@Tunmises-MacBook-Air.local>
2024-11-01 21:36:15 -0400

    navbar fix

--- musematch/src/components/navbar.tsx
+++ musematch/src/components/navbar.tsx
@@ -3,11 +3,11 @@ import { BrowserRouter, Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <div>
-      <BrowserRouter>
+      
         <Link to="/"> Home </Link>
         <Link to="/login"> Login </Link>
         <Link to="/connections"> Connections </Link>
-      </BrowserRouter>
+
     </div>
   );
 };
