c0bd7c68f25da59aa42a46906045e2d26840f587
jessicamayvt <jessicamay@vt.edu>
2024-11-01 21:32:50 -0400

    Fixing issues

--- musematch/src/App.tsx
+++ musematch/src/App.tsx
@@ -10,8 +10,8 @@ function App() {
   return (
     <>
       <div>
-        <Navbar />
         <Router>
+        <Navbar />
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/login" element={<Login />} />
