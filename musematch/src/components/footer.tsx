0f52d6bf9b70f0d114ce1dc2b14adcf00f5234e4
muhayminalam <muhayminalam18@gmail.com>
2024-11-02 03:12:27 -0400

    Add some links

--- musematch/src/components/footer.tsx
+++ musematch/src/components/footer.tsx
@@ -11,14 +11,14 @@ export const Footer: React.FC = () => {
         </div>
         <div className="footer-links">
           <Link to="/" className="footer-link">Home</Link>
-          <Link to="/about" className="footer-link">About Us</Link>
+          <Link to="/about" className="footer-link">About Us </Link>
           <Link to="/contact" className="footer-link">Contact</Link>
           <Link to="/privacy" className="footer-link">Privacy Policy</Link>
         </div>
         <div className="footer-social">
-          <a href="https://facebook.com" className="social-icon" target="_blank" rel="noopener noreferrer">Facebook</a>
-          <a href="https://twitter.com" className="social-icon" target="_blank" rel="noopener noreferrer">Twitter</a>
-          <a href="https://instagram.com" className="social-icon" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a href="https://www.facebook.com/LMCCNYC/" className="social-icon" target="_blank" rel="noopener noreferrer">Facebook</a>
+          <a href="https://x.com/LMCC?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" className="social-icon" target="_blank" rel="noopener noreferrer">Twitter</a>
+          <a href="https://www.instagram.com/lmcc_nyc/?hl=en" className="social-icon" target="_blank" rel="noopener noreferrer">Instagram</a>
         </div>
       </div>
       <div className="footer-bottom">
