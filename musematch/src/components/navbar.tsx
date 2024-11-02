71c6e56dff2c68d56e5f3b41646b691876d9ccbc
Tunmise Kehinde <tunmisekehinde@Tunmises-MacBook-Air.local>
2024-11-01 21:40:31 -0400

    mergefix

--- musematch/src/components/navbar.tsx
+++ musematch/src/components/navbar.tsx
@@ -3,18 +3,10 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <div>
-<<<<<<< HEAD
-      
-        <Link to="/"> Home </Link>
-        <Link to="/login"> Login </Link>
-        <Link to="/connections"> Connections </Link>
-
-=======
       <Link to="/"> Home </Link>
       <Link to="/login"> Login </Link>
       <Link to="/connections"> Connections </Link>
       <Link to="/matching">Matching</Link>
->>>>>>> 6699c8dc705f2ae79f464215483bcfb94fd340ac
     </div>
   );
 };
