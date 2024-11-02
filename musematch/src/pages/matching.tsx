e5b65d90f19d79bf5be9ffb9c2000733d6de152a
jessicamayvt <jessicamay@vt.edu>
2024-11-01 22:26:50 -0400

    Implemented interactive heart/dislike buttons

--- musematch/src/pages/matching.tsx
+++ musematch/src/pages/matching.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FaHeart } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 import logo from "./logo.png";
@@ -5,11 +6,42 @@ import "./Matching.css";
 
 
 export const Matching = () => {
+    //variables for rotation 
+    const [currentX, setCurrentX] = useState(0);
+    const [rotation, setRotation] = useState(0);
     
-    
+    //makes cards automatically swipe right 
+    const handleHeartClick = () => {
+        setCurrentX(1000); // Move off-screen to the right
+        setRotation(20);
+        console.log("User Swiped Right");
+        //
+        setTimeout(() => {
+            setCurrentX(0);
+            setRotation(0);
+          }, 500);
+    };
+
+    // Automatically swipe left
+    const handleXClick = () => {
+    setCurrentX(-1000); // Move off-screen to the left
+    setRotation(-20);
+    console.log("User Swiped Left");
+
+    setTimeout(() => {
+      setCurrentX(0);
+      setRotation(0);
+    }, 500);
+  };
+
     return (
         <div className="App">
-            <div className="card">
+            <div className="card"
+            style={{
+                transform: `translateX(${currentX}px) rotate(${rotation}deg)`,
+                transition: "transform 0.3s ease-out",
+              }}
+            >
             <img src={logo} alt="person" className="pfp-image"/>
             <h1 className="person-name">Name</h1>
             <h2 className="about-me">About me</h2>
@@ -19,8 +51,8 @@ export const Matching = () => {
                 <span className="tag">Art Type</span>
             </div>
             <div className="like-dislike-container">
-                <ImCross className="like-dislike" aria-label="Dislike"/>
-                <FaHeart className="like-dislike" aria-label="Love" />
+                <ImCross className="like-dislike" onClick={handleXClick} aria-label="Dislike"/>
+                <FaHeart className="like-dislike" onClick={handleHeartClick} aria-label="Love" />
             </div>
         </div>
       </div>
