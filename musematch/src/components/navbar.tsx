cc387e8b0e092873349172ec756693aa20369390
muhayminalam <muhayminalam18@gmail.com>
2024-11-01 21:54:03 -0400

    using type script

--- musematch/src/components/navbar.tsx
+++ musematch/src/components/navbar.tsx
@@ -1,23 +1,34 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import "./Navbar.css"; // Import the CSS file for styling
+import { FaHome, FaUser, FaUsers, FaLink } from "react-icons/fa"; // Import icons from react-icons
+import "./Navbar.css";
 
 export const Navbar: React.FC = () => {
   return (
-    <div className="navbar">
+    <nav className="navbar">
+      {/* Logo or Brand Name */}
+      <div className="navbar-brand">
+        <Link to="/" className="nav-link brand">
+          MyApp
+        </Link>
+      </div>
+
       {/* Navigation Links */}
-      <Link to="/" className="nav-link">
-        Home
-      </Link>
-      <Link to="/find-people" className="nav-link">
-        Find People
-      </Link>
-      <Link to="/connections" className="nav-link">
-        Connections
-      </Link>
-      <Link to="/user-profile" className="nav-link">
-        User Profile
-      </Link>
-    </div>
+      <div className="navbar-links">
+        <Link to="/" className="nav-link">
+          <FaHome className="icon" /> Home
+        </Link>
+        <Link to="/find-people" className="nav-link">
+          <FaUsers className="icon" /> Find People
+        </Link>
+        <Link to="/connections" className="nav-link">
+          <FaLink className="icon" /> Connections
+        </Link>
+        <Link to="/user-profile" className="nav-link">
+          <FaUser className="icon" /> User Profile
+        </Link>
+      </div>
+    </nav>
   );
 };
+
