c16a381378eb42d7a09f264b0d07739430ee9d05
Tunmise Kehinde <tunmisekehinde@Tunmises-MacBook-Air.local>
2024-11-02 04:25:02 -0400

    Add login

--- musematch/src/App.tsx
+++ musematch/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Main } from "./pages/main";
-import Login from "./pages/login";
+import LoginPage from "./pages/login";
 import { Navbar } from "./components/navbar";
 import { Connections } from "./pages/connections";
 import { Matching } from "./pages/matching";
@@ -14,7 +14,7 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Main />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/connections" element={<Connections />} />
             <Route path="/matching" element={<Matching />} />
           </Routes>
--- musematch/src/pages/login.tsx
+++ musematch/src/pages/login.tsx
@@ -1,58 +1,98 @@
-import { auth, provider } from "../config/firebase";
-import { signInWithPopup } from "firebase/auth";
-export default function Login() {
-  const signInWithGoogle = async () => {
-    const result = await signInWithPopup(auth, provider);
-    console.log(result);
-  };
+import {
+    Avatar,
+    Box,
+    Container,
+    FormControlLabel,
+    Paper,
+    TextField,
+    Typography,
+    Checkbox,
+    Button,
+    Grid,
+    Link,
+    SvgIcon,
+  } from "@mui/material";
+  import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+  import { Link as RouterLink } from "react-router-dom";
+  import logo from '../assets/LMCC-50Logo-Blue-Smaller.png'
+    import { useState } from "react";
 
-  return (
-    <>
-      {/*
-          This example requires updating your template:
   
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
+  
+  const LoginPage = () => {
+    const [signUp, setSignUp] = useState(false);
+    const handleSubmit = () => console.log("login");
+    return (
+      <Container maxWidth="xs">
+        <Paper elevation={10} sx={{ marginTop: 8, padding: 2}}>
+         
+         <div style={{display: 'flex', justifyContent: 'center', alignItems:'center'}}>
+            <img src={logo} style={{alignSelf: 'center'}} />
+         </div>
+            
+        
+          <Typography component="h1" variant="h5" sx={{ textAlign: "center", marginTop: 2 }}>
+            Sign In
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              placeholder="Enter email"
+              fullWidth
+              required
+              autoFocus
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              placeholder="Enter password"
+              fullWidth
+              required
+              type="password"
+              sx={{ mb: 2 }}
+            />
 
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="Your Company"
-            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
-          />
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
+            {signUp && 
+            <>
+            
+                <TextField
+                placeholder="First Name"
+                fullWidth
+                required
+                sx={{ mb: 2 }}
+                />
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-6">
-            <div>
-              <button
-                onClick={signInWithGoogle}
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in With Google
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm/6 text-gray-500">
-            Not an alumni?{" "}
-            <a
-              href="#"
-              className="font-semibold text-indigo-600 hover:text-indigo-500"
-            >
-              Register Here
-            </a>
-          </p>
-        </div>
-      </div>
-    </>
-  );
-}
+                <TextField
+                placeholder="Last Name"
+                fullWidth
+                required
+                /> 
+            </>}
+            
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button type="submit" variant="contained" fullWidth sx={{ mt: 1 }}>
+                {signUp ? 'Sign up' : 'Log in'}
+            </Button>
+          </Box>
+          <Grid container justifyContent="space-between" sx={{ mt: 1 }}>
+            <Grid item>
+            {!signUp && <>
+              <Link component={RouterLink}>
+                Forgot password?
+              </Link>
+            </>}
+              
+            </Grid>
+            <Grid item>
+              <Link component={RouterLink} onClick={() => setSignUp(!signUp)}>
+              {signUp ? 'Log in' : 'Sign up'}
+              </Link>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
+    );
+  };
+  
+  export default LoginPage;
