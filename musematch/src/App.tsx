060ab4fd9f8ea5aa4bcd3b0b7d1c0bd7844e5c82
Tunmise Kehinde <tunmisekehinde@Tunmises-MacBook-Air.local>
2024-11-01 20:47:07 -0400

    login page back

--- musematch/src/App.tsx
+++ musematch/src/App.tsx
@@ -1,15 +1,16 @@
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Main } from "./pages/main";
-import { Login } from "./pages/login";
+import  Login  from "./pages/login";
 import { Navbar } from "./components/navbar";
 
 function App() {
   return (
     <>
       <div>
-        <Navbar />
+        
         <Router>
+        <Navbar />
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/login" element={<Login />} />
--- musematch/src/pages/login.tsx
+++ musematch/src/pages/login.tsx
@@ -1,15 +1,59 @@
 import { auth, provider } from "../config/firebase";
 import { signInWithPopup } from "firebase/auth";
+export default function Login() {
 
-export const Login = () => {
-  const signInWithGoogle = async () => {
-    const result = await signInWithPopup(auth, provider);
-    console.log(result);
-  };
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth, provider);
+        console.log(result);
+      };
 
-  return (
-    <div>
-      <button onClick={signInWithGoogle}>Sign In With Google</button>
-    </div>
-  );
-};
+    return (
+      <>
+        {/*
+          This example requires updating your template:
+  
+          ```
+          <html class="h-full bg-white">
+          <body class="h-full">
+          ```
+        */} 
+
+        
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm ">
+            <img
+              alt="Your Company"
+              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+              className="mx-auto h-10 w-auto"
+            />
+            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form action="#" method="POST" className="space-y-6">
+             
+  
+              <div>
+                <button
+                onClick={signInWithGoogle}
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign in With Google
+                </button>
+              </div>
+            </form>
+  
+            <p className="mt-10 text-center text-sm/6 text-gray-500">
+              Not an alumni?{' '}
+              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                Register Here
+              </a>
+            </p>
+          </div>
+        </div>
+      </>
+    )
+  }
